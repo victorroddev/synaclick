7426a5f9a6af254a4d3a8b7adb5552c64f451df7
victorroddev <javier.rod.dev@gmail.com>
2025-10-17 18:29:58 -0600

    change route

--- src/main.jsx
+++ src/main.jsx
@@ -18,7 +18,7 @@ const router = createBrowserRouter(([
         element: <HomePage />
       },
       {
-        path: 'test',
+        path: 'invo-dental',
         element: <Invo />
       }
     ]
